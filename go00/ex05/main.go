48cbb27691ae83f90b31451c6b6ebd0cf58af936
Johannes Niedens <johannesniedens@gmail.com>
2026-03-06 15:09:57 -0300

    fix(go00/ex05): case n:1 corrected

--- go00/ex05/main.go
+++ go00/ex05/main.go
@@ -13,18 +13,21 @@ func printError(err error) {
 
 func main() {
 	if len(os.Args) != 2 {
-		printError(fmt.Errorf("Invalid amount of arguments. Required args: 2"))
+		printError(fmt.Errorf("usage: ./ex05 <n>"))
 	}
 	n, err := strconv.Atoi(os.Args[1])
 	if err != nil {
 		printError(err)
 	}
+
+	current, previous := 1, 0
+	if n < 1 {
+		return
+	}
+	fmt.Println(previous)
 	if n < 2 {
 		return
 	}
-
-	current, previous := 1, 0
-	fmt.Println(previous)
 	for i := 0; i < (n - 1); i++ {
 		fmt.Println(current)
 		current, previous = current+previous, current
