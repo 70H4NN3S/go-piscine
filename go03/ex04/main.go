7df621e7ee9dbdce2324d9edb9cea51c96eca4cd
Johannes Niedens <johannesniedens@gmail.com>
2026-03-13 16:32:16 -0300

    feat(PointerPuzzle): I discovered that you can manipulate the original values through s[i] of a slice, because well, it's a pointer

--- go03/ex04/main.go
+++ go03/ex04/main.go
@@ -4,11 +4,15 @@ import "fmt"
 
 func main() {
 	i1 := 42
+	s1 := []int{1, 2, 3, 4, 5, -5, 42}
 	fmt.Println("Before changes:", i1)
 	doublePtr(&i1)
 	fmt.Println("After first call:", i1)
 	i1 = doubleRet(i1)
 	fmt.Println("After second change", i1)
+	fmt.Println("Slice before change:", s1)
+	doubleSlice(s1)
+	fmt.Println("Slice after change:", s1)
 }
 
 func doublePtr(n *int) {
@@ -17,4 +21,10 @@ func doublePtr(n *int) {
 
 func doubleRet(n int) int {
 	return n * 2
+}
+
+func doubleSlice(s []int) {
+	for i := range s {
+		s[i] *= 2
+	}
 }
