047ee14431ae15aba933870e4d5e60bebf7f018d
Johannes Niedens <johannesniedens@gmail.com>
2026-03-14 15:12:28 -0300

    feat(LoggerInterface): created the FileLogger.Log() method that writes the log messages to a file with the given path, or creates a new file if needed

--- go04_Interfaces_Polymorphism/ex01_LoggerInterface/logger.go
+++ go04_Interfaces_Polymorphism/ex01_LoggerInterface/logger.go
@@ -14,15 +14,29 @@ type Logger interface {
 type ConsoleLogger struct{}
 
 type FileLogger struct {
-	File *os.File
+	Path string
 }
 
 type MultiLogger struct {
 	Loggers *[]Logger
 }
 
-func (c ConsoleLogger) Logger(level, message string) {
+func (ConsoleLogger) Log(level, message string) {
 	t := time.Now()
 	fmt.Fprint(os.Stdout, t.Format("2006-01-02 15:04:05"),
 		strings.ToUpper(level), message)
+}
+
+func (f FileLogger) Log(level, message string) {
+	file, err := os.OpenFile(f.Path,
+		os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		cons := ConsoleLogger{}
+		cons.Log("ERROR", "Failed to create the file for the error message")
+		return
+	}
+	defer file.Close()
+
+	errorStr := fmt.Sprint(strings.ToUpper(level), message)
+	file.WriteString(errorStr)
 }
