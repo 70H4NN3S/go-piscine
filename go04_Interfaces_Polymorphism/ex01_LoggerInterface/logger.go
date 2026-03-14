4cae7bad08799a9dd4dbb60077759c56bf93187f
Johannes Niedens <johannesniedens@gmail.com>
2026-03-14 16:59:10 -0300

    feat(LoggerInterface): implemented the FileLogger

--- go04_Interfaces_Polymorphism/ex01_LoggerInterface/logger.go
+++ go04_Interfaces_Polymorphism/ex01_LoggerInterface/logger.go
@@ -14,19 +14,20 @@ type Logger interface {
 type ConsoleLogger struct{}
 
 type FileLogger struct {
-	Path string
+	path string
 }
 
 type MultiLogger struct {
-	Loggers *[]Logger
+	loggers []Logger
 }
 
 func (ConsoleLogger) Log(level, message string) {
-	fmt.Printf("%s [%s] %s\n", time.Now().Format("2006-01-02 15:04:05"), level, message)
+	fmt.Printf("%s [%s] %s\n",
+		time.Now().Format("2006-01-02 15:04:05"), strings.ToUpper(level), message)
 }
 
 func (f FileLogger) Log(level, message string) {
-	file, err := os.OpenFile(f.Path,
+	file, err := os.OpenFile(f.path,
 		os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
 	if err != nil {
 		cons := ConsoleLogger{}
@@ -35,6 +36,6 @@ func (f FileLogger) Log(level, message string) {
 	}
 	defer file.Close()
 
-	errorStr := fmt.Sprint(strings.ToUpper(level), message)
-	file.WriteString(errorStr)
+	fmt.Fprintf(file, "%s [%s] %s\n",
+		time.Now().Format("2006-01-02 15:04:05"), strings.ToUpper(level), message)
 }
