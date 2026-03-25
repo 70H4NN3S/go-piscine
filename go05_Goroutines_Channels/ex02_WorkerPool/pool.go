8b9259ec1e25604efadc3415d31725faa3ad2800
Johannes Niedens <johannesniedens@gmail.com>
2026-03-25 13:08:19 -0300

    feat(WorkerPool): added structure

--- go05_Goroutines_Channels/ex02_WorkerPool/pool.go
+++ go05_Goroutines_Channels/ex02_WorkerPool/pool.go
@@ -1,20 +1,44 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
 type Job struct {
 	ID      int
-	Payload interface{}
+	Payload any
 }
 
 type Result struct {
 	JobID  int
-	Output interface{}
+	Output any
 	Err    error
 }
 
-type Pool struct{}
+type Pool struct {
+	Input  chan Job
+	Result chan Result
+	wg     sync.WaitGroup
+}
 
 func NewPool(workers int) *Pool {
-	return nil
+	input := make(chan Job, 5)
+	result := make(chan Result, 5)
+	p := &Pool{input, result, sync.WaitGroup{}}
+
+	for range workers {
+		p.wg.Add(1)
+		go p.Worker()
+	}
+	return p
+}
+
+func (p *Pool) Worker() {
+	defer p.wg.Done()
+	for job := range p.Input {
+		fmt.Println(job)
+	}
 }
 
 func (p *Pool) Submit(job Job) {
@@ -24,5 +48,8 @@ func Results() <-chan Result {
 	return nil
 }
 
-func Stop() {
+func (p *Pool) Stop() {
+	close(p.Input)
+	p.wg.Wait()
+	close(p.Result)
 }
