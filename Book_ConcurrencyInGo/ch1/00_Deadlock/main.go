e847ca15ae74eb9ba73753472e7a95fa438396db
Johannes Niedens <johannesniedens@gmail.com>
2026-03-25 22:28:02 -0300

    docs(Concurrency): Deadlock

---
+++ Book_ConcurrencyInGo/ch1/00_Deadlock/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type Value struct {
+	mu    sync.Mutex
+	value int
+}
+
+func main() {
+	var wg sync.WaitGroup
+	printSum := func(v1, v2 *Value) {
+		defer wg.Done()
+		v1.mu.Lock()
+		defer v1.mu.Unlock()
+
+		time.Sleep(2 * time.Second)
+		v2.mu.Lock()
+		defer v2.mu.Unlock()
+
+		fmt.Printf("sum=%v\n", v1.value+v2.value)
+	}
+
+	var a, b Value
+	wg.Add(2)
+	go printSum(&a, &b)
+	go printSum(&b, &a)
+	wg.Wait()
+}
