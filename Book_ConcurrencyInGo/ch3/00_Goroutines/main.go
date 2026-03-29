c355f21c6f62830817f11475f6a882a8874bf4c8
Johannes Niedens <johannesniedens@gmail.com>
2026-03-29 10:28:59 -0300

    docs(Concurrency): go routine with waitgroups

--- Book_ConcurrencyInGo/ch3/00_Goroutines/main.go
+++ Book_ConcurrencyInGo/ch3/00_Goroutines/main.go
@@ -1,19 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
-	sayHello := func() {
+	var wg sync.WaitGroup
+	sayHello1 := func() {
+		defer wg.Done()
 		fmt.Println("hello")
 	}
-	sayHello()
-	go sayHello()
-
+	wg.Add(1)
+	go sayHello1()
+	go sayHello2(&wg)
+	wg.Add(1)
 	go func() {
+		defer wg.Done()
 		fmt.Println("hello")
 	}()
+	wg.Add(1)
+	wg.Wait()
 }
 
-func sayHello() {
+func sayHello2(wg *sync.WaitGroup) {
+	defer wg.Done()
 	fmt.Println("hello")
 }
