e45e30e1303d4ce660472a41ab43e2cc3f42a0c3
Johannes Niedens <johannesniedens@gmail.com>
2026-04-06 10:32:38 -0300

    LearnGoWithTests Pointers introduced errors

--- Book_LearnGoWithTests/05_Pointers/wallet.go
+++ Book_LearnGoWithTests/05_Pointers/wallet.go
@@ -17,8 +17,12 @@ func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Withdraw(amount Bitcoin) {
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return fmt.Errorf("only %s available. Can't withdraw %s", w.balance, amount)
+	}
 	w.balance -= amount
+	return nil
 }
 
 func (w *Wallet) Balance() Bitcoin {
--- Book_LearnGoWithTests/05_Pointers/wallet_test.go
+++ Book_LearnGoWithTests/05_Pointers/wallet_test.go
@@ -27,4 +27,16 @@ func TestWallet(t *testing.T) {
 		wallet.Withdraw(Bitcoin(10))
 		assertBalance(t, wallet, Bitcoin(10))
 	})
+
+	t.Run("withdraw insufficient ammount", func(t *testing.T) {
+		startingBalance := Bitcoin(10)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertBalance(t, wallet, startingBalance)
+
+		if err == nil {
+			t.Errorf("Wanted an error but didn't got one")
+		}
+	})
 }
