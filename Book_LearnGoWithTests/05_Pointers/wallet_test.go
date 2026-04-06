4bb898543f69d5c8b700df74d895f397b22ce0d9
Johannes Niedens <johannesniedens@gmail.com>
2026-04-06 10:54:52 -0300

    LearnGoWithTests Pointers refactor taken out the assertion funcs out of the testing func

--- Book_LearnGoWithTests/05_Pointers/wallet_test.go
+++ Book_LearnGoWithTests/05_Pointers/wallet_test.go
@@ -5,26 +5,6 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
-		t.Helper()
-		got := wallet.Balance()
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	}
-
-	assertError := func(t testing.TB, got error, want error) {
-		t.Helper()
-
-		if got == nil {
-			t.Fatal("Wanted an error but didn't got one")
-		}
-
-		if got.Error() != want.Error() {
-			t.Errorf("got %q want %q", got, want)
-		}
-	}
-
 	t.Run("balance", func(t *testing.T) {
 		assertBalance(t, Wallet{balance: Bitcoin(10)}, Bitcoin(10))
 	})
@@ -49,4 +29,24 @@ func TestWallet(t *testing.T) {
 		assertError(t, err, ErrInsufficientFunds)
 		assertBalance(t, wallet, startingBalance)
 	})
+}
+
+func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
+	t.Helper()
+	got := wallet.Balance()
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got == nil {
+		t.Fatal("Wanted an error but didn't got one")
+	}
+
+	if got.Error() != want.Error() {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
